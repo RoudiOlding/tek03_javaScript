17426e33e83f26f6edaa5dd6c0066c1ada7a6af0
RoudiOlding <roudiolding040504@gmail.com>
2025-03-23 21:35:33 -0500

    Updating the code of the js, deleting some stuff like comentaris and unnecesty code

--- 2. proyects/musicRanker/script.js
+++ 2. proyects/musicRanker/script.js
@@ -4,17 +4,17 @@ document.addEventListener('DOMContentLoaded', function() {
     const totalScoreElement = document.getElementById('totalScore');
 
     const maxValues = {
-        escucha: 40, // Rango de 0 a 40
-        gusto: 30,   // Rango de 0 a 30
-        aporte: 20,  // Rango de 0 a 20
-        bonus: 10    // Rango de 0 a 10
+        escucha: 40,
+        gusto: 30,
+        aporte: 20,
+        bonus: 10
     };
 
     // Inicializar los sliders en el medio
     sliders.forEach((slider, index) => {
         const bar = slider.parentElement;
         const barHeight = bar.offsetHeight;
-        slider.style.bottom = `${barHeight / 2}px`; // Iniciar en el medio
+        slider.style.bottom = `${barHeight / 2}px`;
     });
 
     sliders.forEach((slider, index) => {
@@ -28,14 +28,13 @@ document.addEventListener('DOMContentLoaded', function() {
             if (isDragging) {
                 const bar = slider.parentElement;
                 const barRect = bar.getBoundingClientRect();
-                let newPosition = barRect.bottom - e.clientY; // Posición relativa al fondo
+                let newPosition = barRect.bottom - e.clientY;
                 newPosition = Math.max(-15, Math.min(barRect.height + 15, newPosition));
 
                 const maxValue = maxValues[slider.id.replace('Slider', '').toLowerCase()];
                 const step = 5;
                 const value = Math.round((newPosition / barRect.height) * maxValue / step) * step;
 
-                // Asegurar que el valor esté dentro del rango
                 const clampedValue = Math.max(0, Math.min(maxValue, value));
                 slider.style.bottom = `${(clampedValue / maxValue) * barRect.height}px`;
                 scores[index].textContent = clampedValue;
